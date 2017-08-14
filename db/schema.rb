742e02eb333946347b70e947b3410ba4c8de75d8
jameshaggerty <jameshaggerty87@gmail.com>
2017-08-14 12:52:41 -0400

    changed to 5.1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170814112315) do
+ActiveRecord::Schema.define(version: 20170814113553) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20170814112315) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "color"
+    t.string "colour"
   end
 
 end
